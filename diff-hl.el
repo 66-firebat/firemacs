1a4f47ce592fec89be789a7a60eddebc821c6d02
fireshark <66.firebat@gmail.com>
2026-08-11 22:12:10 -0600

    |.git| = 4

--- diff-hl.el
+++ diff-hl.el
@@ -17,7 +17,7 @@
   ;; Left margin display with Nerd Font icons
   (setq diff-hl-margin-symbols-alist
         '((insert . " + ") (delete . " ✕ ") (change . " ↯ ")
-          (unknown . " 󱔢 ") (ignored . " - ") (reference . " ↯ ")))
+          (unknown . "  ") (ignored . " - ") (reference . " ↯ ")))
 
   ;; Enable globally in all file-visiting buffers
   (global-diff-hl-mode 1)
