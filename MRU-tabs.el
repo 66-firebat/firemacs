5225ca32a27fca3f8931c7396c9eda1d5249aa22
fireshark <66.firebat@gmail.com>
2026-09-05 20:25:33 -0600

    changed MRU tab icons

--- MRU-tabs.el
+++ MRU-tabs.el
@@ -104,8 +104,8 @@ tab bar with icons.  All rendering is self-built in raw Emacs Lisp."
     ("Docs"    ""   org-mode markdown-mode text-mode)
     ("Config"  ""   conf-mode)
     ("Dired"   "󰙅"   dired-mode)
-    ("Ghostel" ""   ghostel-mode)
-    ("Broot"   "󰐅"   broot-mode)
+    ("Ghostel" ""   ghostel-mode)
+    ("Broot"   "󰙅"   broot-mode)
     ("Tools"   ""   magit-mode vterm-mode
                help-mode apropos-mode Info-mode)
     ("Buffers" ""))
